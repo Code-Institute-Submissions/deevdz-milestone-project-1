1111fb9b4d2bc8c9dba28f3689559754a2f7f818
Deirdre van der Zee <deirdrevanderzee@gmail.com>
2018-11-10 01:02:07 +0000

    Added Map to Contact Page

---
+++ assets/js/custom-map.js
@@ -0,0 +1,128 @@
+// Custom map
+var google;
+function custom_map() {
+
+var var_location = new google.maps.LatLng(53.4697625, -9.0997838);
+
+var var_mapoptions = {
+center: var_location,
+zoom: 16,
+styles: [
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "hue": "#FFBB00"
+            },
+            {
+                "saturation": 43.400000000000006
+            },
+            {
+                "lightness": 37.599999999999994
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "hue": "#FFC200"
+            },
+            {
+                "saturation": -61.8
+            },
+            {
+                "lightness": 45.599999999999994
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "stylers": [
+            {
+                "hue": "#FF0300"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 51.19999999999999
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "hue": "#FF0300"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 52
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "hue": "#0078FF"
+            },
+            {
+                "saturation": -13.200000000000003
+            },
+            {
+                "lightness": 2.4000000000000057
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "hue": "#00FF6A"
+            },
+            {
+                "saturation": -1.0989010989011234
+            },
+            {
+                "lightness": 11.200000000000017
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    }
+]
+
+};
+
+var var_map = new google.maps.Map(document.getElementById("map-container"),
+var_mapoptions);
+
+var var_marker = new google.maps.Marker({
+position: var_location,
+map: var_map,
+title: "Headford Wellbeing Centre"
+});
+}
+
+// Initialize maps
+google.maps.event.addDomListener(window, 'load', custom_map);
